20205925cd9055fe53082481fd417dbb3029d2c4
ede <837221025@qq.com>
2020-10-16 19:01:12 +0800

    第一次搞

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+    <el-table
+      :data="tableData"
+      height="200"
+      border
+      stripe
+      size= "mini"
+      style="width: 100%" >
+      <el-table-column
+        align="center"
+        prop="number"
+        label="id"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        align="center"
+        prop="name"
+        label="名称"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        align="center"
+        prop="remarks"
+        label="备注"
+        width="180">
+      </el-table-column>
+       <el-table-column 
+        align="center"
+        prop="operate"
+        label="操作"
+        width="180">
+      </el-table-column>
+     
+      
+    </el-table>
+  </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+  <script>
+    export default {
+      data() {
+        return {
+          tableData: [{
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+            number: '1',
+            name: '张三',
+            remarks: '这是好人',
+            operate: '修改 删出',
+          }, {
+            number: '2',
+            name: '李四',
+            remarks: '脾气大',
+            operate: '修改 删出',
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+          }, {
+            number: '3',
+            name: '王五',
+            remarks: '爱生气',
+            operate: '修改 删出',
+          },{
+            number: '4',
+            name: '赵六',
+            remarks: '游手好闲',
+            operate: '修改 删出',
+          },{
+            number: '5',
+            name: '王二',
+            remarks: '一事无成',
+            operate: '修改 删出',
+          },{
+            number: '6',
+            name: '麻子',
+            remarks: '好吃懒做',
+            operate: '修改 删出',
+          }]
+        }
+      }
+    }
+  </script>
