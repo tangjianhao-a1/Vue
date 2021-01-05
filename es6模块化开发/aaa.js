8eff08a1f4c160b8943d6934be10a812a2458d44
ede <837221025@qq.com>
2021-01-05 12:28:43 +0800

    模块化开发

---
+++ es6模块化开发/aaa.js
@@ -0,0 +1,13 @@
+var name = '小明页面';  
+var age = 18;    
+var flag = true;  
+function sum (num1,num2) {
+  return num1 + num2
+}; 
+
+if(flag) {
+  console.log(sum(20,30));
+};  
+
+//export default sum  
+export {name,age,flag,sum}
---
+++ es6模块化开发/bbb.js
@@ -0,0 +1,7 @@
+
+//import fn from "./aaa.js"    
+import * as aaa from "./aaa.js"
+   
+ console.log(aaa.name);  
+ console.log(aaa.age);
+
